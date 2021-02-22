7e23eb3bcf00069edc4ac7810183bea830a3e0d7
Andy James <andy@andyjames.org>
2021-02-22 14:02:11 -0600

    first commit

---
+++ main.tf
@@ -0,0 +1,34 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_ami" "centos" {
+  owners      = ["679593333241"]
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["CentOS Linux 7 x86_64 HVM EBS *"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.centos.id
+  instance_type = var.size
+
+  tags = {
+    Name = var.tag_name
+    TTL = var.tag_ttl
+    Owner = var.tag_owner
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,14 @@
+
+variable "tag_name" {
+  type = string
+}
+variable "tag_ttl" {
+  type = string
+}
+variable "tag_owner" {
+  type = string
+}
+variable "size" {
+  type = string
+  default = "t2.large"
+}
